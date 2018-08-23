aee801c6d632ca5b013cc2cf09de0f9265297e56
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-23 01:41:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,4 +130,26 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+var prefix = "+";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+ 
+ 
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
